cc344c7fe683b77692f7a8080693ac320482a410
Ananya Singh <ananyasingh2405s@gmail.com>
2026-06-09 22:00:40 +0530

    Added LeetCode daily with explanation

--- 2026-06-09_solution.java
+++ 2026-06-09_solution.java
@@ -1,3 +1,22 @@
+/*
+Problem: LeetCode Daily Challenge
+Link: https://leetcode.com/problems/maximum-total-subarray-value-i?envType=daily-question&envId=2026-06-09
+
+Idea:
+- We need to find the maximum possible total value of k subarrays.
+- Each subarray value is (max - min).
+- In this solution, we use global max and min of the array.
+
+Approach:
+- Step 1: Find maximum element in array
+- Step 2: Find minimum element in array
+- Step 3: Compute (max - min)
+- Step 4: Multiply by k
+
+Mistake / Learning:
+- Learned how to structure GitHub problem logs properly.
+*/
+
 class Solution {
     public long maxTotalValue(int[] nums, int k) {
         int maxi = Integer.MIN_VALUE;
